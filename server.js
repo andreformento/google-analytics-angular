92a326b349bbeed9467ea78fc631d03e96857742
André Formento <andreformento.sc@gmail.com>
2017-01-11 18:06:01 -0200

    nodemon

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-console.log("aaaaaaaaaaaaaaaaaaaaaa");
+
+console.log("Init App");
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
